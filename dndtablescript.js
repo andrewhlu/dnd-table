784462db0447871d670cc0567ec8900fb3462f08
Max LR <mleeroller@gmail.com>
2020-01-11 03:05:25 -0800

    Grid prototyped

--- dndtablescript.js
+++ dndtablescript.js
@@ -1,9 +1,38 @@
+currSize = 100;
+
 window.addEventListener('load', function() {
   document.querySelector('input[type="file"]').addEventListener('change', function() {
       if (this.files && this.files[0]) {
           var img = document.querySelector('img');  // $('img')[0]
           img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-          img.onload = imageIsLoaded;
       }
   });
 });
+
+function createGrid(size) {
+    var gridStatus = document.getElementById("gridDisplay");
+
+    if(gridStatus) {
+      gridStatus.remove();
+    }
+
+    currSize = size;
+    var ratioW = Math.floor($(window).width()/size),
+        ratioH = Math.floor($(window).height()/size);
+    
+    var parent = $('<div />', {
+        id: 'gridDisplay',
+        class: 'grid', 
+        width: ratioW  * size, 
+        height: ratioH  * size
+    }).addClass('grid').appendTo('body');
+
+    for (var i = 0; i < ratioH; i++) {
+        for(var p = 0; p < ratioW; p++){
+            $('<div />', {
+                width: size - 1, 
+                height: size - 1
+            }).appendTo(parent);
+        }
+    }
+}
