2921336908347110df66d507e230d725f894f668
Max LR <mleeroller@gmail.com>
2020-01-11 01:30:18 -0800

    moved js to own file

---
+++ dndtablescript.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');  // $('img')[0]
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          img.onload = imageIsLoaded;
+      }
+  });
+});
+
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+// Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+// Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+
+    // Creates rows
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+
+// Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
