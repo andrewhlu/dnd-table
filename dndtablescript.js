b80dea6a0ffe97ddc13b2065f8ded836528b7f26
Max LR <mleeroller@gmail.com>
2020-01-11 01:41:10 -0800

    Image upload (non firebase) working, beginning grid

--- dndtablescript.js
+++ dndtablescript.js
@@ -7,34 +7,3 @@ window.addEventListener('load', function() {
       }
   });
 });
-
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
-
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
