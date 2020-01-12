f79f4b4e731d3f698da17e498fc6c68e768b6472
jango99 <ja.ngo7199@gmail.com>
2020-01-12 02:33:00 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,43 +1,46 @@
 import pyautogui, random, json, time
-
 from websocket_server import WebsocketServer
 
-rightSide = 1920
-leftSide = 0
-bottomSide = 0
-topSide = 1080
 tick = 0
-stage = 'finger-up'
+stage = "finger-up"
 tapDefine = 1000 #upperbound to define tap in ms. Anything above this value is considered a hold
 tapLength = 0
 
 
-xCord = random.randint(leftSide, rightSide)
-yCord = random.randint(bottomSide, topSide)
-
-pyautogui.moveTo(xCord,yCord)
-print(str(xCord) + ' ' + str(yCord))
+#xCord = random.randint(leftSide, rightSide)
+#yCord = random.randint(bottomSide, topSide)
+#pyautogui.moveTo(xCord,yCord)
+#print(str(xCord) + ' ' + str(yCord))
 
 def new_client(client, server):
 	print("Connection Established")
 
 def message_received(client, server, message):
+	global stage, tick, tapLength, tapDefine
 	print(json.loads(message))
 	result = json.loads(message)
-	if "finger-down" in result.type and if stage not in result.type:
-		stage = 'finger-down'
-		tick = result.time
-		print("tap started")
+	if "calibration" in result["type"]:
+		frameWidth = result["frame"]["width"] #width dimension of user calibrated play area
+		frameHeight = result["frame"]["height"] #height dimension of user calibrated play area
+		windowWidth = result["window"]["width"] #width dimension of entire screen
+		windowHeight = result["window"]["height"] #height dimension of entire screen
 
-	elif "finger-up" in result.type and if stage not in result.type:
-		stage = 'finger-up'
-		tapLength = result.time - tick
-		if tapLength < tapDefine:
-			pyautogui.click()
-			print("tap registered")
-		elif:
-			 print("tap no longer registered, hold is registered ")#when you hold for longer than 1s, it will register a drag
-			 
+	else:
+		if "finger-down" in result["type"]: 
+			pyautogui.moveTo(result["position"]["x"], result["position"]["y"])
+			if stage not in result["type"]:
+				stage = 'finger-down'
+				tick = result["time"]
+				print("tap started")
+			
+
+		elif "finger-up" in result["type"] and stage not in result["type"]:
+			stage = 'finger-up'
+			tapLength = result["time"] - tick
+
+			if tapLength < tapDefine:
+				pyautogui.click()
+				print("tap registered")
 
 
 
